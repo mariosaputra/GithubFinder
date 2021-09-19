3c2b8d6a13b1d81d1862478ee71cbecf7453f7c1
Mario Saputra <41255803+mariosaputra@users.noreply.github.com>
2021-09-19 14:47:30 +0000

    addEventListener to search form

---
+++ app.js
@@ -0,0 +1,11 @@
+//Search Input
+
+const searchUser = document.getElementById('search-field');
+
+searchUser.addEventListener('keyup', (e) => {
+  const searchUsername = e.target.value;
+
+  if(searchUsername != ''){
+    
+  }
+})
